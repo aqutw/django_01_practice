dbecbaaa1ef0083eda9cec047dc300bef86320ec
aqutw <aquarian.ex@gmail.com>
2015-04-02 21:26:47 +0800

    Create tripModel AND Add trip into INSTALLED_APP AND python manage.py migrate && python manage.py makemigrations(for making trip) && python manage.py migrate(for execute trip migration)

--- trips/models.py
+++ trips/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# see also https://docs.djangoproject.com/en/1.7/ref/models/fields/
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    photo = models.URLField(blank=True)
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+
