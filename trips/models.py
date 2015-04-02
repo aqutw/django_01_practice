3875bf61ae6696274a82acecec6735a40b5af7de
aqutw <aquarian.ex@gmail.com>
2015-04-02 21:36:04 +0800

    FIX unicode text for Python 2.7

--- trips/models.py
+++ trips/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.encoding import force_unicode # http://stackoverflow.com/questions/3798137/djangounicodedecodeerror-and-force-unicode
 
 # see also https://docs.djangoproject.com/en/1.7/ref/models/fields/
 class Post(models.Model):
@@ -9,4 +10,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
+    def __unicode__(self):
+        #return force_unicode(self.title)
+        return u'%s' % self.title # http://mozillazg.com/2013/05/django-__unicode__-bad-unicode-data.html
 
